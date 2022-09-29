b9b0fd3fcd00fa8019b223d0e5f6e0161fa2b87a
ShwetaKesare <113175158+ShwetaKesare@users.noreply.github.com>
2022-09-29 14:19:21 +0530

    Add files via upload

---
+++ angularassignment/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { ItemhttpService } from './service/itemhttp.service';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
